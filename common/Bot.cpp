7372b8fa6f31786af98288f8fdf1e4ec27335072
zm990217 <1040741834@qq.com>
2019-07-15 01:17:08 +0800

    common层

---
+++ common/Bot.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Bot.h"
+
+
+CBot::CBot()
+{
+}
+
+CBot::~CBot()
+{
+}
+
+CBot::CBot(float x, float y, PCTSTR szImgName) : CTankEntry(x, y, szImgName)
+{
+}
---
+++ common/Bot.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "TankEntry.h"
+
+class CBot : public CTankEntry 
+{
+public:
+	CBot();
+	CBot(float x, float y, PCTSTR szImgName);
+	virtual ~CBot();
+};
